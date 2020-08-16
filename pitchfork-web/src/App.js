d55fd42afe6d9238fc13bf625f6e12763e1c9fbf
Gaetano Checinski <gaetano.checinski@gmail.com>
2020-08-16 16:54:40 +0100

    small design improvements

--- pitchfork-web/src/App.js
+++ pitchfork-web/src/App.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Link, Switch, Route } from 'react-router-dom';
+import { Link, Switch, Route, useLocation } from 'react-router-dom';
 import { get } from 'superagent';
+import RadarChart from 'react-svg-radar-chart';
+import 'react-svg-radar-chart/build/css/index.css';
 
 
 function Steps(props) {
   return <div>
-    <b>Preparation</b>
-    <div> Ready in: {props.readyInMinutes}min </div>
-    <div> Preparation: {props.preparationMinutes}min </div>
-    <div> Cooking: {props.cookingMinutes}min </div>
+    <h2>Preparation</h2>
     {
       props.analyzedInstructions.map(x =>
         <div>
@@ -26,7 +25,7 @@ function Steps(props) {
 function Ingredients (props) {
   return (
     <div>
-      <b>Ingredients</b>
+      <h2>Ingredients</h2>
       <ul>
         {props.extendedIngredients.map( (x, id) =>
           <li key={id}>{x.originalString} </li>
@@ -36,32 +35,103 @@ function Ingredients (props) {
   );
 }
 
+
+const Radar = ({title, data}) => (
+  <RadarChart
+    options = {{size:'33%', scales:5}}
+    title = {title}
+    captions = {
+      data
+        .map(x=>x.title)
+        .reduce((a,b)=> ({[b]:b, ...a}), {})
+    }
+    data = {[{
+      meta: {color: 'orange'},
+      data: data
+        .reduce((a,b) => ({[b.title]: Math.min(1,b.value/100), ...a}), {})}]
+    } />
+);
+
+
+function MacroBreakdown({caloricBreakdown, nutrients}) {
+  const {percentProtein, percentFat, percentCarbs} = caloricBreakdown
+
+  const carbs = nutrients.find(x => x.title == 'Net Carbohydrates') || {amount: 0};
+  const fiber = nutrients.find(x => x.title == 'Fiber') || {amount: 0};
+  const sugar = nutrients.find(x => x.title == 'Sugar') || {amount: 0};
+  const protein = nutrients.find(x => x.title == 'Protein') || {amount: 0};
+  const fat = nutrients.find(x => x.title == 'Fat') || {amount: 0};
+  const saturated = nutrients.find(x => x.title == 'Saturated Fat') || {amount: 0};
+
+
+  const saturatedPercent = percentFat * saturated.amount / fat.amount;
+  const unsaturatedPercent = percentFat * (1 - saturated.amount / fat.amount);
+  const fiberPercent = percentCarbs * fiber.amount / carbs.amount
+  const sugarPercent = percentCarbs * sugar.amount  / carbs.amount
+  const complexPercent = percentCarbs * (carbs.amount - sugar.amount)  / carbs.amount
+
+  const data = [
+    {title: "Unsaturated Fat", value: unsaturatedPercent, color: "black"},
+    {title: "Saturated Fat", value: saturatedPercent, color: "darkgrey"},
+    {title: "Protein", value: percentProtein, color: "blue"},
+    {title: "Fiber", value: fiberPercent, color: "green"},
+    {title: "Sugar", value: sugarPercent, color: "red"},
+    {title: "Complex Carbs", value: complexPercent, color: "orange"},
+  ];
+
+  const scale = Math.max(...data.map(x=>x.value));
+  const scaled = data.map(x=>({...x, value: x.value/scale * 100}));
+
+  return <Radar title="Macros" data={scaled} />
+}
+
+const summaryExcludes = new Set([
+  'Net Carbohydrates',
+  'Cholesterol',
+]);
+
 function Nutrition (props) {
+  const excludes = new Set([
+    'Carbohydrates',
+    'Net Carbohydrates',
+    'Fat',
+    'Sugar',
+    'Fiber',
+    'Calories',
+    'Saturated Fat',
+    'Cholesterol',
+    'Protein',
+    'Folate'
+  ]);
+
+  const nutrients = props
+    .nutrition
+    .nutrients
+    .filter(x=>!excludes.has(x.title))
+    .sort((a,b) => a.title.localeCompare(b.title) )
+
+  const vitamins = nutrients
+    .filter(x => x.title.includes('Vitamin'))
+    .map(x=>({...x, value: Math.min(100, x.percentOfDailyNeeds)}));
+
+  const minerals = nutrients
+    .filter(x => !x.title.includes('Vitamin') )
+    .map(x=>({...x, value: Math.min(100, x.percentOfDailyNeeds)}));
+
   return (
-    <div>
-      <table>
-        <thead>
-          <tr><th>Nutrition</th> <td>Amount</td> <td>RDA</td> </tr>
-        </thead>
-        <tbody>
-          {props
-            .nutrition
-            .nutrients
-            .sort((a,b) => b.amount - a.amount)
-            .map( ({title, amount, unit, percentOfDailyNeeds}, key)=>
-              <tr key={key}>
-                  <td>{title}</td>
-                  <td>{amount}{unit}</td>
-                  <td>{percentOfDailyNeeds}%</td>
-              </tr>
-          )}
-        </tbody>
-      </table>
-      <ul>
-        <li> Protein: {props.nutrition.caloricBreakdown.percentProtein}% </li>
-        <li> Fat: {props.nutrition.caloricBreakdown.percentFat}% </li>
-        <li> Carbs: {props.nutrition.caloricBreakdown.percentCarbs}% </li>
-      </ul>
+    <div className="Nutrition">
+      <div>
+        <h2>Vitamins RDA %</h2>
+        <Radar title="Vitamins" data={vitamins} />
+      </div>
+      <div>
+        <h2>Minerals RDA %</h2>
+        <Radar title="Minerals" data={minerals} />
+      </div>
+      <div>
+        <h2>Macros</h2>
+        <MacroBreakdown {...props.nutrition} />
+      </div>
     </div>
   );
 }
@@ -77,31 +147,17 @@ function RecipeCard({name}) {
 
   return (
       <div className="card" >
-        <div className="info">
-
-          <div className="meta">
-            <span title="Ready in">
-            &#8986;{data.readyInMinutes}mins
-            </span>
-
-            <span title="servings">
-              &#127869; {data.servings}
-            </span>
-          </div>
-        </div>
-
-        <div className="title">{data.title}</div>
-
         <Link to={name} className="image">
           <img src={data.image} />
         </Link>
 
-        <a href={data.sourceUrl} className="source">{
-          data.sourceUrl
-            .replace(/https?:\/\//, '')
-            .split('/')
-            .find(x => x)
-        }</a>
+        <div className="meta">
+          <div className="source">{
+            data.creditsText
+          }</div>
+          <div className="title">{data.title}</div>
+        </div>
+
 
       </div>
   );
@@ -121,9 +177,50 @@ function Recipe({match}) {
     .from({length:3}, (_, i) => Math.floor(Math.random() * 1000))
     .map(i => `keto-${i}`);
 
+
+
+  const additionalTags = [
+    data.veryPopular && 'popular',
+    data.veryHealthy && 'very healthy',
+  ].filter(x=>x);
+
+  const top5Nutrients = data
+    .nutrition
+    .nutrients
+    .filter( x => !summaryExcludes.has(x.title))
+    .filter( x => x.percentOfDailyNeeds > 100)
+    .sort( (a,b) => b.percentOfDailyNeeds - a.percentOfDailyNeeds)
+    .slice(0, 5);
+
+    console.log(data);
   return (
     <>
-      <img src={data.image} />
+      <div className="header">
+        <div  style={{padding: '1em'}}>
+          <img src={data.image}/>
+        </div>
+        <div>
+          <h1>{data.title}</h1>
+          <ul className="summaryList">
+            <li> <a href={data.sourceUrl}>{data.sourceUrl}</a> </li>
+            <li> Servings: <b>{data.servings}</b></li>
+            <li> Ready in: <b>{data.readyInMinutes}</b>min </li>
+            {data.preparationMinutes &&
+              <li> Preparation time: <b>{data.preparationMinutes}</b>min </li> }
+            {data.cookingMinutes &&
+              <li> Cooking time: <b>{data.cookingMinutes}</b>min </li> }
+            <li> Tags: {
+              data.dishTypes
+                .concat(data.diets)
+                .concat(additionalTags)
+                .map( x => <span key={x} className="tag">{x} </span> )
+            }</li>
+
+            <li>High in: {top5Nutrients.map(x => <span key={x.title} className="tag">{x.title.replace('Vitamin ', '')} </span> )} </li>
+
+          </ul>
+        </div>
+      </div>
       <Nutrition {...data} />
       <Ingredients {...data} />
       <div>
@@ -141,6 +238,16 @@ function Recipe({match}) {
   );
 }
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   const recipes = Array
     .from({length:20}, () => Math.floor(Math.random() * 1000))
@@ -149,14 +256,45 @@ function App() {
 
   return (
     <>
+      <ScrollToTop/>
       <div className="Navigation">
         <Link to="/"> FoodWall </Link>
       </div>
       <Switch>
         <Route exact path="/">
-          <div className="grid">{
-            recipes.map( (name, key) => <RecipeCard name={name} key={key} />)
-          }</div>
+          <div className="banner">
+            <div className="tagline">
+              <p>
+                Be the Hero of the next Dinner
+              </p>
+              <p>
+                Discover new Recipes and their Nutrition Value
+              </p>
+            </div>
+            <div className="copic">
+              <img src="images/platter-1932466_1920.jpg" />
+            </div>
+          </div>
+{/*
+          <div className="section videos">
+            <h2>Featured Videos</h2>
+            <div className="grid">
+              <iframe width="300" height="300"  src="https://www.youtube.com/embed/zrRDnLJdjmQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe width="300" height="300"  src="https://www.youtube.com/embed/zrRDnLJdjmQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe width="300" height="300"  src="https://www.youtube.com/embed/zrRDnLJdjmQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe width="300" height="300"  src="https://www.youtube.com/embed/zrRDnLJdjmQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+*/}
+          <div className="section">
+            <h2> Recipe Wall </h2>
+            <div className="grid">
+              {
+              recipes.map( (name, key) => <RecipeCard name={name} key={key} />)
+            }</div>
+
+          </div>
+
         </Route>
         <Route path="/:id" component={Recipe}  />
       </Switch>
