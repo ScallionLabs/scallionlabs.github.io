d063bc222706ed6474ab71b83e16b58baf56ded9
Gaetano Checinski <gaetano.checinski@gmail.com>
2020-08-18 12:23:48 +0100

    Updates

---
+++ precache-manifest.61761e0414ccf6873747de0b79a909da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63edbca98746511f6da8b6812ceda2b5",
+    "url": "/foodwall/index.html"
+  },
+  {
+    "revision": "9d05718444dd7c6a3fb9",
+    "url": "/foodwall/static/css/2.301005e7.chunk.css"
+  },
+  {
+    "revision": "49d4539c314afd2a3b61",
+    "url": "/foodwall/static/css/main.e4a6ff6f.chunk.css"
+  },
+  {
+    "revision": "9d05718444dd7c6a3fb9",
+    "url": "/foodwall/static/js/2.3482983b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/foodwall/static/js/2.3482983b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d4539c314afd2a3b61",
+    "url": "/foodwall/static/js/main.b49c5d93.chunk.js"
+  },
+  {
+    "revision": "54071ab00c302877250c",
+    "url": "/foodwall/static/js/runtime-main.2376b3fe.js"
+  }
+]);
