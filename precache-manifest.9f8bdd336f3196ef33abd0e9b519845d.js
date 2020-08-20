7e07d1a1bbf5ee7e259df19be2d0e2bb27db2ee3
Gaetano Checinski <gaetano.checinski@gmail.com>
2020-08-20 15:05:09 +0100

    Updates

---
+++ precache-manifest.9f8bdd336f3196ef33abd0e9b519845d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46672f2d3940db82aa6aae0b736d281f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0303fd0a60e2f3aeec4",
+    "url": "/static/css/2.301005e7.chunk.css"
+  },
+  {
+    "revision": "eb58380caa45566c2ec1",
+    "url": "/static/css/main.4e553614.chunk.css"
+  },
+  {
+    "revision": "d0303fd0a60e2f3aeec4",
+    "url": "/static/js/2.2c242af4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2c242af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb58380caa45566c2ec1",
+    "url": "/static/js/main.14cf3530.chunk.js"
+  },
+  {
+    "revision": "2bbbd620737a1a308e93",
+    "url": "/static/js/runtime-main.ec212785.js"
+  }
+]);
